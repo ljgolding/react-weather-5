a136d8fb031a09ca71ce50a950de3e2cbf9c5af9
ljgolding <101936562+ljgolding@users.noreply.github.com>
2022-06-23 06:44:32 -0600

    Fix reference

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Weather App
-    This project was coded by Lindsay Golding and is {" "} <a href="https://github.com/ljgolding/react-weather-5" target="_blank">open-sourced on Github</a>
-
-    </div>);
+      Weather App This project was coded by Lindsay Golding and is{" "}
+      <a
+        href="https://github.com/ljgolding/react-weather-5"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        open-sourced on Github
+      </a>
+    </div>
+  );
 
 }
 
