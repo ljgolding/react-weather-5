1a984d004c8443560c6df48088094f78efe7d771
ljgolding <101936562+ljgolding@users.noreply.github.com>
2022-06-24 09:07:19 -0600

    fix typo

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -5,7 +5,7 @@ export default function FormattedDate(props) {
     let day =days[props.date.getDay()];
     let hours= props.date.getHours();
 
-    if (hours <10) { hours-`0${hours}`;}
+    if (hours <10) { hours=`0${hours}`;}
 
     let minutes=props.date.getMinutes();
 
