e67134aa1a2fdf7c96cd757af38555f84d3e6edb
ljgolding <101936562+ljgolding@users.noreply.github.com>
2022-06-23 18:47:05 -0600

    Added CSS

--- src/Weather.js
+++ src/Weather.js
@@ -3,5 +3,52 @@ import "./Weather.css";
 
 
 export default function Weather(){
-    return <div className="Weather">Hi</div>;
+    return (
+      <div className="Weather">
+       <form> 
+        <div className="row">
+            <div className="col-9">
+        
+                <input
+                    type="search"
+                    placeholder="Enter a city.."
+                    className="form-control"
+                />
+             </div>
+          <div className="col-3">
+          <input type="submit" value="search" className="btn btn-primary"/>
+          </div>
+          </div>
+        </form>
+        
+
+        <h1>New York</h1>
+        <ul>
+          <li> Wednesday 07:00</li>
+          <li> Mostly Cloudy</li>
+        </ul>
+        <div className="row mt-3">
+          <div className="col-6">
+            <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Partly Cloudy"
+              className="float-left"
+            />  
+            
+   
+           <span className="temperature"> 6</span>
+           <span className="unit">°C</span>
+        </div>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li> Precipitation: 15%</li>
+              <li> Humitidty: 72%</li>
+              <li> Wind: 13 km/h</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
 }
